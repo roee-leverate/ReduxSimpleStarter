a83b5495adf5b01485a4494c83148c0b2842a0bb
Unknown <roee.cohen@leverate.com>
2018-02-28 12:00:24 +0200

    Finished proj.
    Added Google maps
    Added design
    \etc...

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import { connect } from 'react-redux'
-import { Sparklines, SparklinesLine } from 'react-sparklines'
+import Chart from '../components/Chart'
+import GoogleMap from '../components/GoogleMap'
 
 class WeatherList extends Component {
 
@@ -13,16 +14,20 @@ class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name
     const temps = cityData.list.map(weather => weather.main.temp)
-    console.log(temps);
+    const pressures = cityData.list.map(weather => weather.main.pressure)
+    const humidities = cityData.list.map(weather => weather.main.humidity)
+    //const lon = cityData.city.coord.lon
+    //const lat = cityData.city.coord.lat
+    const {lon, lat} = cityData.city.coord
+
+    console.log(lon, lat);
 
     return (
       <tr key={name}>
-        <td>{name}</td>
-        <td>
-          <Sparklines height={120} width={180} data={temps}>
-            <SparklinesLine color="red"/>
-          </Sparklines>
-        </td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
+        <td><Chart data={temps} color="orange" units="K"/></td>
+        <td><Chart data={pressures} color="green" units="hPa" /></td>
+        <td><Chart data={humidities} color="blue" units="%" /></td>
       </tr>
     )
   }
